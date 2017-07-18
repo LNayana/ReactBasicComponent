6e33e673a27fc1ec26ad2a5be5bedf51c358a155
LNayana <lokhandenayana22@gmail.com>
2017-07-18 12:58:03 +0530

    removed unnecessary files and added react basic component demo snippet

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+class Hello extends React.Component{
+    render(){
+        return (
+            <h1>Hello</h1>
+        );
+    }
+}
+
+class World extends React.Component{
+    render(){
+        return(
+        <h1>World</h1>
+        );
+    }
+}
+
+class HelloWorld extends React.Component{
+    render(){
+        return (
+        <div>
+            <Hello />
+            <World />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<HelloWorld/>,document.getElementById('root'));
