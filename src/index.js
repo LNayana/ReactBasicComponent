51d872d00da34b2a4ec1125b0afb451f0f85032d
LNayana <lokhandenayana22@gmail.com>
2017-07-18 19:57:44 +0530

    learnt to add data to a list

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import TodoItem from './components/TodoItem.js'; // relative path
+import TodoForm from './components/TodoForm.js'; // relative path
 
 class TodoList extends React.Component{
     constructor(){
         super();
         this.changeStatus=this.changeStatus.bind(this);
+        this.updateTask=this.updateTask.bind(this);
+        this.addTask=this.addTask.bind(this);
         this.state={
             tasks:[
                 {
@@ -25,9 +28,31 @@ class TodoList extends React.Component{
                  name:'Buy Snacks',
                     completed:false
                 }
-            ]
+            ],
+            currentTask:''
         }
     }
+    
+    addTask(evt){
+        evt.preventDefault();
+        let tasks=this.state.tasks;
+        let currentTask=this.state.currentTask;
+        tasks.push({
+            name:currentTask,
+            completed:false
+        })
+        this.setState({
+            tasks,
+            currentTask:''
+        })
+    }
+    
+    updateTask(newValue){
+        this.setState({
+            currentTask:newValue.target.value
+        })
+    }
+    
     changeStatus(index){
         console.log(this.state.tasks[index]);
         //Below code to toggle this.state.tasks.completed value
@@ -37,8 +62,11 @@ class TodoList extends React.Component{
 //set the modified value into original this.state to reflect the changes 
 this.setState({tasks:tasks});        
     }
+    
     render(){
         return (
+            <section>
+            <TodoForm currentTask={this.state.currentTask} updateTask={this.updateTask} addTask={this.addTask} />
             <ul>
             {
                 this.state.tasks.map((task,index) => {
@@ -47,6 +75,7 @@ this.setState({tasks:tasks});
                 
             }
             </ul>
+</section>
         );
     }
 }
